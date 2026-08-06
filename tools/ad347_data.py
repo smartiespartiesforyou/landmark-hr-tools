cdd1a45241bd8802542ee38a02659fd25c075199
smartiespartiesforyou <smartiespartiesforyou@gmail.com>
2026-08-06 09:59:42 -0500

    Add function to build AD347 record

---
+++ tools/ad347_data.py
@@ -0,0 +1,7 @@
+def build_ad347_record(name, department, work_date):
+    return {
+        "facility": "Landmark South Nursing & Rehabilitation",
+        "employee_name": name,
+        "department": department,
+        "date": work_date,
+    }
