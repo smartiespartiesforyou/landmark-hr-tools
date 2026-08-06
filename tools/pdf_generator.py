6a08b73606b7d73cbd7406b4ae18b6d242471af8
smartiespartiesforyou <smartiespartiesforyou@gmail.com>
2026-08-06 10:08:48 -0500

    Refactor create_ad347 for clearer parameter usage

--- tools/pdf_generator.py
+++ tools/pdf_generator.py
@@ -1,4 +1,9 @@
 from tools.ad347_data import build_ad347_record
 
+
 def create_ad347(name, department, work_date):
-    return build_ad347_record(name, department, work_date)
+    return build_ad347_record(
+        name=name,
+        department=department,
+        work_date=work_date,
+    )
